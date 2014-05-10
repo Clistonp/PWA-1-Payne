2f66294dce4f9651fdac193c12d6b204fc4af95b
Cliston Payne <Clistonp@yahoo.com>
2014-05-10 17:38:25 -0400

    Setting Up variables

--- WK1/goal1_assign_duel1/JS/main.js
+++ WK1/goal1_assign_duel1/JS/main.js
@@ -2,3 +2,26 @@
    05/10/2014
    Assignment: The Duel #1
  */
+
+
+//self executing function
+(function(){
+    console.log("fight");
+//player names
+    var playerOneName = "Kabal";
+    var playerTwoName = "Kratos";
+
+
+//player Damage
+    var playerOneDamage = 20;
+    var playerTwoDamage = 20;
+
+
+//player Health
+    var playerOneHealth = 100;
+    var playerTwoHealth = 100;
+
+
+
+
+})();
