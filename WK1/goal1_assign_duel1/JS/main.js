d424b4f0e30a2061ca3f9cb9becfeffdfc2845e1
Cliston Payne <Clistonp@yahoo.com>
2014-05-10 18:44:58 -0400

    Finished Debugging Now

--- WK1/goal1_assign_duel1/JS/main.js
+++ WK1/goal1_assign_duel1/JS/main.js
@@ -22,10 +22,10 @@
     var playerTwoHealth = 100;
 
 //Round
-    var round = 1;
+    var round = 0;
 
     function fight(){
-        console.log("in the fight function");
+        //console.log("in the fight function");
         alert(playerOneName+":"+playerOneHealth+" *START* "+playerTwoName+":"+playerTwoHealth);
 
         for (var i=0;  i < 10; i++){
@@ -34,18 +34,47 @@
            var minDamage2 = playerTwoDamage * .5;
            var f1 = Math.floor(Math.random()*(playerOneDamage-minDamage1)+minDamage1);
            var f2 = Math.floor(Math.random()*(playerTwoDamage-minDamage2)+minDamage2);
-            console.log(f1);
-            console.log(f2);
+            //console.log(f1);
+            //console.log(f2);
+         //inflict Damage
+            playerOneHealth -= f1;
+            playerTwoHealth -= f2;
+            //console.log(playerOneHealth);
+            //console.log(playerTwoHealth);
+           console.log(playerOneName+":"+playerOneHealth+"  "+playerTwoName+":"+playerTwoHealth);
+
+            var results = winnerCheck();
+            console.log(results);
+
+            if (results === "No Winner") {
+                round++;
+                alert(playerOneName + ":" + playerOneHealth + " *ROUND " + round + " OVER* " + playerTwoName + ":" + playerTwoHealth);
+            }else{
+                alert(results);
+                break;
+            }
+        }
+
+    }
 
 
 
-        };
-        winnerCheck()
-    };
+    function winnerCheck() {
+        console.log("in winnerCheck fn")
 
+        var results = "No Winner";
 
+        if (playerOneHealth < 1 && playerTwoHealth < 1) {
+            result = "You Both are Dead";
+        } else if (playerOneHealth < 1) {
+            result = playerTwoName + "WINS!!!"
+        } else if (playerTwoHealth < 1) {
+            result = playerOneName + "WINS!!!"
+        }
 
-    function winnerCheck(){}
+        return results;
+    }
+
     console.log("program start")
     fight();
 
