c3c7f7598f23a54c79b4e3ab25c60da1f91ee0b9
Cliston Payne <Clistonp@yahoo.com>
2014-05-10 18:09:08 -0400

    Setting Up player Damage variables

--- WK1/goal1_assign_duel1/JS/main.js
+++ WK1/goal1_assign_duel1/JS/main.js
@@ -21,7 +21,33 @@
     var playerOneHealth = 100;
     var playerTwoHealth = 100;
 
+//Round
+    var round = 1;
 
+    function fight(){
+        console.log("in the fight function");
+        alert(playerOneName+":"+playerOneHealth+" *START* "+playerTwoName+":"+playerTwoHealth);
+
+        for (var i=0;  i < 10; i++){
+
+           var minDamage1 = playerOneDamage * .5;
+           var minDamage2 = playerTwoDamage * .5;
+           var f1 = Math.floor(Math.random()*(playerOneDamage-minDamage1)+minDamage1);
+           var f2 = Math.floor(Math.random()*(playerTwoDamage-minDamage2)+minDamage2);
+            console.log(f1);
+            console.log(f2);
+
+
+
+        };
+        winnerCheck()
+    };
+
+
+
+    function winnerCheck(){}
+    console.log("program start")
+    fight();
 
 
 })();
