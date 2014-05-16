b96dcde98981498171a231cb68ba68641f52b41f
Cliston Payne <Clistonp@yahoo.com>
2014-05-16 16:01:32 -0400

    Creating Variables

--- WK2/goal4_assign_guessing_game/js/main.js
+++ WK2/goal4_assign_guessing_game/js/main.js
@@ -9,7 +9,15 @@
  */
 
 //Game variables
-(function ()
+(function (){
+    //Variables for guesses
+    var playerGuess = 0;
+    var remainingGuess = 3;
+    var guessesMade = 0;
+
+
+    //Variable that has the computer choose a random number
+    var computersNum = Math.floor((Math.random() * 10) + 1);
 
 
 })();
