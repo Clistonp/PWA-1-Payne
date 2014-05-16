357a3c993046af16b1e36719293fdc148c00bd0f
Cliston Payne <Clistonp@yahoo.com>
2014-05-16 16:14:07 -0400

    Click guess function made

--- WK2/goal4_assign_guessing_game/js/main.js
+++ WK2/goal4_assign_guessing_game/js/main.js
@@ -17,7 +17,26 @@
 
 
     //Variable that has the computer choose a random number
+    //The number 1 is the computers starting point and 10 is the max number
     var computersNum = Math.floor((Math.random() * 10) + 1);
+
+    //Defining the DOM Elements as objects
+    var dom ={
+        input: document.querySelector("input"),
+        output: document.querySelector("output"),
+        button: document.querySelector("button")
+
+    };
+      // Console logs the number of guess the player has remaining
+    var clickfn = function(e){
+        console.log(remainingGuess);
+        validateInput();
+
+    };
+
+
+
+
 
 
 })();
