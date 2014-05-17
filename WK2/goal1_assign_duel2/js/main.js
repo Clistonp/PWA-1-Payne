bc6e3387ea4ad788172632c9221834b75b9092d6
Cliston Payne <Clistonp@yahoo.com>
2014-05-16 20:53:23 -0400

    commiting my first set of arrays

--- WK2/goal1_assign_duel2/js/main.js
+++ WK2/goal1_assign_duel2/js/main.js
@@ -1,34 +1,32 @@
+/*Name: Cliston Payne
 
+Assignment: Duel #2
+
+Date:5/16/14
+
+*/
 //self executing function
 (function(){
     console.log("Fight!!!");
-//player names
-    var playerOneName = "Kabal";
-    var playerTwoName = "Kratos";
+
+//player names damage and health array
+    var fighter1 = ["Kabal", 20, 100];
+    var fighter2 = ["Kratos", 20, 100];
 
 
-//player Damage
-    var playerOneDamage = 20;
-    var playerTwoDamage = 20;
-
-
-//player Health
-    var playerOneHealth = 100;
-    var playerTwoHealth = 100;
-
-//Round
+//Round number
     var round=0;
 
     function fight(){
         //console.log("in the fight function");
-        alert(playerOneName+":"+playerOneHealth+" *START* "+playerTwoName+":"+playerTwoHealth);
+        alert(fighter1[0]+":"+fighter1[2]+" *START* "+fighter2[0]+":"+fighter2[2]);
 
         for (var i = 0;  i < 10; i++){
 
-            var minDamage1 = playerOneDamage * .5;
-            var minDamage2 = playerTwoDamage * .5;
-            var f1 = Math.floor(Math.random()*(playerOneDamage-minDamage1)+minDamage1);
-            var f2 = Math.floor(Math.random()*(playerTwoDamage-minDamage2)+minDamage2);
+            var minDamage1 = fighter1[1] * .5;
+            var minDamage2 = fighter2[1] * .5;
+            var f1 = Math.floor(Math.random()*(fighter1[1]-minDamage1)+minDamage1);
+            var f2 = Math.floor(Math.random()*(fighter2[2]-minDamage2)+minDamage2);
             //console.log(f1);
             //console.log(f2);
             //inflict Damage
