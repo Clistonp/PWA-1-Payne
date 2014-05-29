2ff9266ff8dbc538d369c9dbebfa2d883e81a226
Cliston Payne <Clistonp@yahoo.com>
2014-05-29 12:55:18 -0400

    Creating Variables

--- WK4/goal7_assign_obj_to_prototypes/js/main.js
+++ WK4/goal7_assign_obj_to_prototypes/js/main.js
@@ -6,3 +6,13 @@
 // Cliston Payne
 // 05/29/14
 // Goal 7 Advance Objects
+
+(function(){
+
+    //Defining the Variablees
+    var numPeople = 3;
+    var peeps = [];
+    var names = ["Cliston", "Cooper", "Mckenna", "Gwen", "Ragnar", "John Snow", "Stephanie"];
+    var interval;
+
+}
