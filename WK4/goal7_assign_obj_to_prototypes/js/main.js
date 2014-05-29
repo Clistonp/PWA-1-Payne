0b55e9d6cffbbbb4035ee62014103e82d5f8244f
Cliston Payne <Clistonp@yahoo.com>
2014-05-29 13:10:50 -0400

    Setting up for update of actions by the persons

--- WK4/goal7_assign_obj_to_prototypes/js/main.js
+++ WK4/goal7_assign_obj_to_prototypes/js/main.js
@@ -15,4 +15,34 @@
     var names = ["Cliston", "Cooper", "Mckenna", "Gwen", "Ragnar", "John Snow", "Stephanie"];
     var interval;
 
+    // creating a loop that creates 3 person objects perload
+    for (var z = 0; z < numPeople; z++) {
+
+        //Generate a number up to the names.lengthwhich is 5
+        var peepsIndex = Math.floor(Math.random()* names.length);
+
+        //use a keyword to set up a new person
+
+        var person = new Person(names[peepsIndex], z + 1);
+
+        populateHTML(person.name,"r" + (z + 1) + "c1");
+        populateHTML(person.job, "r" + (z + 1) + "c2");
+
+        people.push(person);
+        names.splice(peepsIndex, 1);
+
+    }
+    //makes sure only one interval is running at a time and clears out the old one in time for the new one
+    clearInterval(interval);
+
+    //set the interval to 30 seconds
+    // set interval will continue to call tg function until the clear interval is called or the window is closed
+    interval = setInterval(runUpdate, 1000 / 30);
+
+    function populateHTML(data,field){
+        var id = documnet.getElementById(field);
+
+    }
+
+
 }
