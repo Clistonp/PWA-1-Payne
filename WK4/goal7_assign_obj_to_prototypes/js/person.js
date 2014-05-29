717f3434190d6a202de2dac409ce8528dbbede6f
Cliston Payne <Clistonp@yahoo.com>
2014-05-29 12:51:16 -0400

    Defining the rest of the objects and creating the random generators

--- WK4/goal7_assign_obj_to_prototypes/js/person.js
+++ WK4/goal7_assign_obj_to_prototypes/js/person.js
@@ -7,7 +7,7 @@
 // 05/29/14
 // Goal 7 Advance Objects Person Blueprint
 
-(function)(){
+(function(){
     //added the Person object to the global window-- main.js file will need this
     window.Person = Person;
     //giving actions and jobs for the person instances
@@ -24,6 +24,25 @@
             //set job to be random
             this.job = Person.jobs[Math.floor(Math.random()*Person.jobs.length)];
 
-            //
+            //define the row so JS knows what row in the HTML to update
+            this.row = row;
+
+            //display the first action
+            var id = documnet.getElementById("r" + this.row + "c3");
+            id.innerHTML = this.action;
+
+        };
+    //Person's update method This happens 30 times a second and randomly determines if the action changes for each person
+
+    Person.prototype.update = function(){
+        if (Math.floor(Math.random() <.01)){
+            var i = Math.floor(Math.random()* Person.action.length);
+            this.action = Person.action[i];
+            var id = document.getElementById("r" + this.row + "c3");
+            id.innerHTML = this.action;
+
         }
-}
+    }
+
+
+})();
