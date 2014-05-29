995612370d3206bea4fae9462b2bdbd8c94c3400
Cliston Payne <Clistonp@yahoo.com>
2014-05-29 12:32:35 -0400

    creating function

--- WK4/goal7_assign_obj_to_prototypes/js/person.js
+++ WK4/goal7_assign_obj_to_prototypes/js/person.js
@@ -5,4 +5,13 @@
  */
 // Cliston Payne
 // 05/29/14
-// Goal 7 Person Blueprint
+// Goal 7 Advance Objects Person Blueprint
+
+(function)(){
+    //added the Person object to the global window-- main.js file will need this
+    window.Person = Person;
+    //giving actions and jobs for the person instances
+    Person.jobs = ["Marine","Police Officer","Dental Assistant","Construction Foremen","Teacher" ];
+    Person.actions = ["Training", "Break","Resting","Working"];
+
+}
